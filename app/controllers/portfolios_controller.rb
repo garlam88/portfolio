620121b4bc67e0d2d34817896adee4c0d3aa2ccb
Garry <Garry@Garrys-iMac.local>
2019-02-08 10:41:36 -0500

    Refactored before action for portfolio testing

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout "portfolio"
   def index
     @portfolio_items = Portfolio.all
@@ -26,11 +27,9 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
    end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
@@ -42,12 +41,10 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def destroy
     # Perform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
 
     # Destroy/delete the record
     @portfolio_item.destroy
@@ -56,6 +53,10 @@ class PortfoliosController < ApplicationController
     respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
+  end
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])    
   end
 
 end
