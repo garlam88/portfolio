1d64b678aac879326b4c7c228685c549dd9faf38
Garry <Garry@Garrys-iMac.local>
2019-02-21 10:31:54 -0500

    Refactored alerts into help methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
+
 module ApplicationHelper
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
@@ -58,5 +59,18 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert 
+    end
+  end
+
+  def alert_generator msg
+    js add_gritter(msg, title: "Garry Lam Portfolio", sticky: false) 
+  end
+
 end
+
 
